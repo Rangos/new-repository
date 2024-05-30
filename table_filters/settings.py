26f4eb9783c3a9b2cd36f205d9280bd8d1340cce
Rangos <emmanuelmarang9@gmail.com>
2024-05-30 11:58:29 +0300

    Update settings.py

--- table_filters/settings.py
+++ table_filters/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb1',
         'USER': 'root',
-        'PASSWORD': 'Manu1985$',  
+        'PASSWORD': 'password',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
          'OPTIONS': {  
