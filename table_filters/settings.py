a7431257d10490cb27526823997a8e0f9a31bbe0
Rangos <emmanuelmarang9@gmail.com>
2024-06-23 21:50:55 +0300

    Update settings.py

--- table_filters/settings.py
+++ table_filters/settings.py
@@ -84,14 +84,14 @@ WSGI_APPLICATION = 'table_filters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-       # 'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
- #   }
-#}
-
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb1',
@@ -103,7 +103,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
         }  
    }
-}
+} """"
 
 #CACHES
 CACHES = {
